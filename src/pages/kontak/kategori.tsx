97c7cec8079af82b8502a861eef0bd1c709be2ff
Diska Esfandiary Sabilillah <diskaesfandiary22@gmail.com>
2024-05-28 22:27:07 +0700

    FEAT: Build new feat

---
+++ src/pages/kontak/kategori.tsx
@@ -0,0 +1,14 @@
+import LayoutAdmin from '@/components/Layout'
+import React from 'react'
+
+const kategori = () => {
+  return (
+    <>
+    <LayoutAdmin>
+        hello
+    </LayoutAdmin>
+    </>
+  )
+}
+
+export default kategori
---
+++ src/pages/pengaturan/visimisi.tsx
@@ -0,0 +1,14 @@
+import LayoutAdmin from '@/components/Layout'
+import React from 'react'
+
+const informasiAdmin = () => {
+  return (
+    <>
+    <LayoutAdmin>
+        hello 
+    </LayoutAdmin>
+    </>
+  )
+}
+
+export default informasiAdmin
